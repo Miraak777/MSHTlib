88687fe28e264622e7237928da2fa63a8d6209c2
Fortie <miraakbs@gmail.com>
2024-05-21 18:40:59 +0300

    feat: added setup file

--- architecture/create_scripts.py
+++ architecture/create_scripts.py
@@ -3,8 +3,3 @@ from os import mkdir
 
 def create_scripts():
     mkdir("scripts")
-
-# def create_bash_script():
-#     with open("local_ci.sh") as bash_script_file:
-#         bash_script_file.write(
-#         )
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mshtlib",
+    version="0.1.0",
+    author="Miraak777",
+    author_email="miraakbs@gmail.com",
+    description="Library for creating micro services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Miraak777/MSHTlib",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "coverage==7.5.1",
+        "black==24.4.2",
+        "isort==5.13.2",
+        "pylint==3.2.2",
+    ],
+)
