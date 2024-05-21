628f2b9dee7d1ded1dee1be557794b9a51c8c2c6
Fortie <miraakbs@gmail.com>
2024-05-21 18:55:32 +0300

    fix: added package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Miraak777/MSHTlib",
-    packages=find_packages(),
+    packages=find_packages('architecture'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
