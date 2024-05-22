26959fa033ee31a9a84498af54aab5e6f797f423
Fortie <miraakbs@gmail.com>
2024-05-22 22:59:54 +0300

    fix: some fixes

--- mshtlib/architecture/create_scripts.py
+++ mshtlib/architecture/create_scripts.py
@@ -1,8 +1,13 @@
-# from shutil import copytree
-# from pathlib import Path
-# from mshtlib.common_constants import PROJECT_DIR
-from os import mkdir, name
+from shutil import copy
+from pathlib import Path
+from mshtlib.common_constants import PROJECT_DIR, SystemType
+from platform import system
+from os import mkdir
 
 
 def create_scripts():
-    mkdir(name)
+    mkdir("scripts")
+    if system() == SystemType.WINDOWS:
+        copy(PROJECT_DIR.joinpath("architecture", "scripts", "cmd", "local_ci.cmd"), Path("scripts"))
+    elif system() == SystemType.LINUX:
+        copy(PROJECT_DIR.joinpath("architecture", "scripts", "bash", "local_ci.sh"), Path("scripts"))
