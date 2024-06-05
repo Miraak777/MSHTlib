d80227265066a54668bf88c4bdf7b7f0635f4ead
Fortie <miraakbs@gmail.com>
2024-06-05 15:39:57 +0300

    fix: some fixes

--- mshtlib/architecture/component.py
+++ mshtlib/architecture/component.py
@@ -6,5 +6,5 @@ from shutil import copytree, copy
 def create_component():
     copytree(PROJECT_DIR.joinpath("architecture", "component_template", "component"), Path("src").joinpath("components", "component_template"))
     copy(PROJECT_DIR.joinpath("architecture", "component_template", "test_example_component.py"), Path("tests").joinpath("components"))
-    with open(Path("src").joinpath("components", "__init__.py")) as file:
-        file.write("from src.components.components_template.component import ComponentExample")
+    with open(Path("src").joinpath("components", "__init__.py"), "a") as file:
+        file.write("from src.components.components_template.component import ComponentExample\n")
